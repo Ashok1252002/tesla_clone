330b9604efa37c4d47416ffb6d7bc24f2bc763c4
Ashok Raj <ashokrajkoribilli@gmail.com>
2021-09-05 18:33:24 +0530

    added header

---
+++ components/CarItems/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import styles from "./styles";
+
+const CarItem = () => {
+	return (
+		<View style={styles.carContainer}>
+			{/* Header */}
+			<View style={styles.header}>
+				<Text>Settings</Text>
+				<Text>AshMobile</Text>
+				<Text>Surprise</Text>
+			</View>
+		</View>
+	);
+};
+
+export default CarItem;
---
+++ components/CarItems/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+	carContainer: {
+		width: "100%",
+		height: "100%",
+		backgroundColor: "orange",
+		color: "white",
+	},
+	header: {
+		marginTop: 50,
+		marginLeft: 20,
+		marginRight: 20,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+});
+
+export default styles;
